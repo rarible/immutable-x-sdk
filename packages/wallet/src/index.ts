4f0d4b491b3d2c987bd534e089b4fffabd401aa9
vitalbadjo <cueage@gmail.com>
2022-03-31 10:38:37 +0400

    fix: remove wallet func

--- packages/wallet/src/index.ts
+++ packages/wallet/src/index.ts
@@ -1,7 +1,3 @@
-import { Link } from "@imtbl/imx-link-sdk"
-import type { RaribleImxEnv } from "@rarible/immutable-sdk/build/config/domain"
-import { RARIBLE_IMX_ENV_CONFIG } from "@rarible/immutable-sdk/build/config/env"
-
 // const link = new Link('https://link.ropsten.x.immutable.com')
 
 // const LOCAL_STORAGE_IMMUTABLE_WALLET_ADDRESS_KEY = "IMM_WALLET_ADDRESS"
@@ -14,11 +10,7 @@ import { RARIBLE_IMX_ENV_CONFIG } from "@rarible/immutable-sdk/build/config/env"
 // }
 // const address = localStorage.getItem(LOCAL_STORAGE_IMMUTABLE_WALLET_ADDRESS_KEY);
 
-export function createImxWallet(env: RaribleImxEnv) {
-	const link = new Link(RARIBLE_IMX_ENV_CONFIG[env].linkAddress)
-	return {
-		connect: async () => {
-			return await link.setup({})
-		},
-	}
+export function createImxWallet() {
+	return false
+
 }
