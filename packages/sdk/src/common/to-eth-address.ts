1d7b1744dad39de8a7bb6e2cc42e84dcece01116
vitalbadjo <cueage@gmail.com>
2022-05-18 17:10:25 +0300

    fix: fix personalSign function

--- packages/sdk/src/common/to-eth-address.ts
+++
@@ -1,8 +0,0 @@
-import type { ImmutableXClient } from "@imtbl/imx-sdk"
-
-type ImxBrandedEthAddress = Parameters<ImmutableXClient["getBalances"]>[0]["user"]
-
-export function toEthAddress(address: string): ImxBrandedEthAddress {
-
-	return address as ImxBrandedEthAddress
-}
